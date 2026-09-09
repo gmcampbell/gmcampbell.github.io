0bf8a23803e882e585724ef0f92990d92f59c349
gmcampbell <57533401+gmcampbell@users.noreply.github.com>
2026-09-09 17:50:10 +0000

    Deploying to gh-pages from @ gmcampbell/gmcampbell.github.io@3049b152160d2fa0da226238fcd77cf362546bc3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,18 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-spa-lifting",
+        },{id: "projects-spa-lifting",
           title: 'SPA lifting',
           description: "Mechanical design of soft pneumatic actuators (SPA) for physical human-robot interaction (pHRI).",
           section: "Projects",handler: () => {
@@ -59,16 +48,6 @@ ninja.data = [{
           description: "A foray into UI development to query physical media.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-scholar',
         title: 'Google Scholar',
